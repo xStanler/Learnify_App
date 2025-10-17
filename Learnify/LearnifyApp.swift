9e35e243b5dd2331a947a99dbfe7a515a6fe234c
Stanisław Chmielewski <kontakt.stanislaw.chmielewski@gmail.com>
2025-10-17 14:33:02 +0200

    Initial Commit

---
+++ Learnify/LearnifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnifyApp.swift
+//  Learnify
+//
+//  Created by Stanisław Chmielewski on 17/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearnifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnifyTests/LearnifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnifyTests.swift
+//  LearnifyTests
+//
+//  Created by Stanisław Chmielewski on 17/10/2025.
+//
+
+import Testing
+@testable import Learnify
+
+struct LearnifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
