b42b323b661ac4a38c2fb9311bec021f244cc44e
Stanisław Chmielewski <kontakt.stanislaw.chmielewski@gmail.com>
2025-12-03 20:05:22 +0100

    Cleared out older garbage from project.

---
+++ Learnify/MainPage.swift
@@ -0,0 +1,20 @@
+//
+//  MainPage.swift
+//  Learnify
+//
+//  Created by Stanisław Chmielewski on 03/12/2025.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        ZStack {
+            Color("#ffffff")
+        }
+    }
+}
+
+#Preview {
+    MainPage()
+}
